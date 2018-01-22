33bedde3adb335775c7e620169471a494e5666a3
isyazriena <33883961+isyazriena@users.noreply.github.com>
2018-01-22 10:19:18 +0800

    task completed

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -1,13 +1,19 @@
+//student name: intan syazriena binti mohd. shahidon
+//student id: a17dw2281
 //include your name and matric number here
 //display the string below using the string function state below.
 
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 int main()
 {
-	char engkau[10];
+	char engkau[10] = "a";
+	char aku[10] = "Hello";
 
 	//stringcopy(a,"hello");
+	strcpy (engkau, aku);
 	
 	cout<<engkau;
 
